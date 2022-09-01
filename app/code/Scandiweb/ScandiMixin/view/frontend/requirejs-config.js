3284decdbf1de8d511f95dc4a789893f1f346b0f
Maksym Tarabara <maksym.tarabara@scandiweb.com>
2022-09-01 14:19:03 +0300

    Task 8: Remove confirmation popup when deleting products from minicart.

---
+++ app/code/Scandiweb/ScandiMixin/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/sidebar': {
+                'Scandiweb_ScandiMixin/js/sidebar-mixin': true
+            }
+        }
+    }
+};
